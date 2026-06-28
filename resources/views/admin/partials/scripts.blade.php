56646be0bb696aaaf86b18ab4a99f41f05cbb9e3
kholif18 <rahmadnur.kholifatur@gmail.com>
2026-06-28 23:02:43 +0700

    refactor: separate admin assets to public/admin/, use local Tabler files

--- resources/views/admin/partials/scripts.blade.php
+++ resources/views/admin/partials/scripts.blade.php
@@ -1,7 +1,3 @@
-<!--begin::Javascript-->
-<script>
-    var hostUrl = "{{ asset('') }}";
-</script>
 <script src="{{ asset('frontend/js/app.js') }}"></script>
-<script src="https://cdn.jsdelivr.net/npm/tabler@1.0.0-beta16/dist/js/tabler.min.js"></script>
+<script src="{{ asset('admin/js/tabler.min.js') }}"></script>
 
